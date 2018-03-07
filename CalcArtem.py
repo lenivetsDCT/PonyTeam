c5c2a6f01f2c72ce0e9af5085ebdbfecd2f37f95
dct.leni <unconfigured@null.spigotmc.org>
2018-03-08 01:05:40 +0200

    New test

--- CalcArtem.py
+++ CalcArtem.py
@@ -1,7 +1,16 @@
 # x = input("Input x: ")
 # print(x)
 
-x = int(input("Enter x: "))
-y = int(input("Enter y: ")) 
-sum = x + y
-print('Sum: ', sum, "LOLO")
+# x = int(input("Enter x: "))
+# y = int(input("Enter y: ")) 
+# sum = x + y
+# print('Sum: ', sum, "LOLO")
+
+list = []
+while True:
+    x = input("Enter brand: ")
+    if (len(x) > 5): 
+        list.append(x)
+    if x == '':
+         break
+print(list)

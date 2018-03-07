d70cdad682f099be8ed03bcd1b03947180ec9a28
UncleTj <bes91@bigmir.net>
2018-03-08 01:45:02 +0200

    asd

--- CalcSasha.py
+++ CalcSasha.py
@@ -2,9 +2,10 @@ list = []
 while True:
     x = input("Марка машины")
     y = int(input("Сколько КМ в день?"))
-    if y > 50: 
+    sum = y * 5
+    if y > 5: 
+    if (sum > 50):
         list.append(y)
     if y == '':
         break
-    sum = y * 5
-    print(x , sum)
+    print(list)

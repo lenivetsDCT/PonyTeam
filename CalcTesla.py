ceed5ba585f5889480f4e3afe3ffee1358aef6f3
denisinberg <denisinberg@gmail.com>
2018-03-08 01:53:01 +0200

    Zaebis

--- CalcTesla.py
+++ CalcTesla.py
@@ -2,9 +2,9 @@ list = []
 while True:
     x = input("Какая модель авто?: ")
     if x=='':
+        break
     y = int(input("Сколько км до работы?"))
     sum = y * 5
     if (sum > 50):
        list.append(x)
-       break
 print(list)

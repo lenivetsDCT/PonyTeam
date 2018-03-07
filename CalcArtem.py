646442a65600e094a540bc5e8b55027ef09c863f
dct.leni <unconfigured@null.spigotmc.org>
2018-03-08 00:19:45 +0200

    Right way

--- CalcArtem.py
+++ CalcArtem.py
@@ -1,4 +1,9 @@
-x = int(input("Enter x: "))
-y = int(input("Enter y: ")) 
-sum = x + y
-print('Sum: ', sum, "LOLO")
+x = input("Input x: ")
+print(x)
+
+
+
+# x = int(input("Enter x: "))
+# y = int(input("Enter y: ")) 
+# sum = x + y
+# print('Sum: ', sum, "LOLO")

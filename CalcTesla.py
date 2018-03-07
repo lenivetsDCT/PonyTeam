f97ff9e7bc19f2bede8b36d1bca330123a5cc9ce
denisinberg <denisinberg@gmail.com>
2018-03-08 01:37:53 +0200

    Vesr 111

--- CalcTesla.py
+++ CalcTesla.py
@@ -1,10 +1,10 @@
 list = []
 while True:
-    y = input("Какая модель авто?: ")
-    x = int(input("Сколько км до работы?"))
+    x = input("Какая модель авто?: ")
+    y = int(input("Сколько км до работы?"))
     sum = y * 5
-    if (len(x) > 50):
+    if (len(y) > 50):
        list.append(x)
     if x=='':
        break
- print(list)
+print(list)
